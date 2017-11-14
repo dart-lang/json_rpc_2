f050c8f97d657856c5a45b3307bd009e4f3399d5
Natalie Weizenbaum <nweiz@google.com>
2017-11-14 14:16:28 -0800

    Use pumpEventQueue() from test

--- test/server/stream_test.dart
+++ test/server/stream_test.dart
@@ -9,8 +9,6 @@ import 'package:test/test.dart';
 
 import 'package:json_rpc_2/json_rpc_2.dart' as json_rpc;
 
-import 'utils.dart';
-
 void main() {
   var requestController;
   var responseController;
