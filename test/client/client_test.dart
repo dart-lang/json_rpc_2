b8dfe403fd8528fd14399dee3a6527b55802dd4d
Sam Rawlins <srawlins@google.com>
2020-10-28 14:44:14 -0700

    Remove unused dart:async imports. (#67)
    As of Dart 2.1, Future/Stream have been exported from dart:core.

--- test/client/client_test.dart
+++ test/client/client_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:test/test.dart';
 import 'package:json_rpc_2/error_code.dart' as error_code;
 import 'package:json_rpc_2/json_rpc_2.dart' as json_rpc;
